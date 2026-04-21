ac868e9b505fdeb101af2d80efce19a01e8bfbb1
shhriiii <149945420+shhriiii@users.noreply.github.com>
2026-04-21 14:38:05 +0530

    updtd spnsr

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -6,7 +6,7 @@ const Sponsors = () => {
 
       <div className="sponsor-container">
         <img src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Inae_logo_resized.jpg" alt="sponsor" />
-        <img src="https://pbs.twimg.com/media/G42CUH-XoAAy4iZ.jpg" alt="sponsor" />
+        {/* <img src="https://pbs.twimg.com/media/G42CUH-XoAAy4iZ.jpg" alt="sponsor" /> */}
         {/* <img src="https://via.placeholder.com/120x60" alt="sponsor" />
         <img src="https://via.placeholder.com/120x60" alt="sponsor" /> */}
       </div>
